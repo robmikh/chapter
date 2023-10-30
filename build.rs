4a342fafb5ce0ea0de37ede4a2b8acef27c5e3e8
robmikh <rob.mikh@outlook.com>
2023-10-29 20:04:10 -0700

    does this work?

--- build.rs
+++ build.rs
@@ -3,24 +3,26 @@ use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(&["src/yarn_spinner.proto"], &["src/"])?;
 
-    for entry in std::fs::read_dir("test_files").unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if let Some(extension) = path.extension() {
-            if let Some(extension) = extension.to_str() {
-                if extension == "yarn" {
-                    let yarn_path = path.to_str().unwrap();
-                    println!("cargo:rerun-if-changed={}", yarn_path);
-                    let status = std::process::Command::new("ysc.exe")
-                            .args([
-                                "compile",
-                                "--output-directory",
-                                "test_files",
-                                yarn_path,
-                            ])
-                            .status()
-                            .unwrap();
-                        assert!(status.success());
+    if cfg!(test) {
+        for entry in std::fs::read_dir("test_files").unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if let Some(extension) = path.extension() {
+                if let Some(extension) = extension.to_str() {
+                    if extension == "yarn" {
+                        let yarn_path = path.to_str().unwrap();
+                        println!("cargo:rerun-if-changed={}", yarn_path);
+                        let status = std::process::Command::new("ysc.exe")
+                                .args([
+                                    "compile",
+                                    "--output-directory",
+                                    "test_files",
+                                    yarn_path,
+                                ])
+                                .status()
+                                .unwrap();
+                            assert!(status.success());
+                    }
                 }
             }
         }
